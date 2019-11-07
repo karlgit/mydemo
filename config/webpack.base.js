0d3c2f7182c9f276dcba896fe16d8c385e78eb7c
lzj <583101334@qq.com>
2019-11-07 14:35:15 +0800

    fix some bug

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -56,8 +56,8 @@ module.exports = {
   // This is important because it allows us to avoid bundling all of our
   // dependencies, which allows browsers to cache those libraries between builds.
   externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
+    "React": "react",
+    "ReactDOM": "react-dom"
   },
   plugins: [new CleanWebpackPlugin()],
 
